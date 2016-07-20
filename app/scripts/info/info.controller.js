9f50b12c3db3951f56ab75861e416ac15d955825
Keni Barwick <keni@barwick.net>
2016-07-20 13:41:11 +0100

    Added the information tab

---
+++ app/scripts/info/info.controller.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('chilled.info')
+		.controller('InfoController', InfoController);
+
+	InfoController.$inject = ['$scope', 'infoService', 'motion'];
+
+	/* @ngInject */
+	function InfoController($scope, infoService, motion) {
+	
+			
+	
+	}
+})();
